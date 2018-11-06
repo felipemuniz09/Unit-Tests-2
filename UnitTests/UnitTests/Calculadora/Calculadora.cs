a6031c8836f16513774c6b6bec86949dca293d7c
felipemuniz09 <felipemuniz09@gmail.com>
2018-11-05 22:09:59 -0200

    testes de calculadora

---
+++ UnitTests/UnitTests/Calculadora/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTests.Calculadora
+{
+    public class Calculadora
+    {
+        public int Dividir(int a, int b, out int resto)
+        {
+            resto = a % b;
+
+            return a / b;
+        }
+    }
+}
---
+++ UnitTests/UnitTests/Calculadora/CalculadoraTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace UnitTests.Calculadora
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Dividir_Sucesso()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+            int resto;
+
+            // Act
+            var resultado = calculadora.Dividir(20, 7, out resto);
+
+            // Assert
+            Assert.Equal(2, resultado);
+            Assert.Equal(6, resto);
+        }
+    }
+}
